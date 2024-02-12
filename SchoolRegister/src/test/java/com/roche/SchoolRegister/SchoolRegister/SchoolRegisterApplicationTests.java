14328df0c077b0a59d06360252750083b0321c00
rocheanthony02 <rocheanthony02@gmail.com>
2024-02-12 19:45:30 -0400

    creating project

---
+++ SchoolRegister/src/test/java/com/roche/SchoolRegister/SchoolRegister/SchoolRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roche.SchoolRegister.SchoolRegister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolRegisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
